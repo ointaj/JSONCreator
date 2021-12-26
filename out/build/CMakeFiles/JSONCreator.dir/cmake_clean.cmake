bf98845129a75996d608bf976feb106b2a9cc173
ointaj <sintaj.oliver1@gmail.com>
2021-12-26 16:06:43 +0100

    Step(8) Chaned: Addded missing directories

---
+++ out/build/CMakeFiles/JSONCreator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JSONCreator.dir/src/main.cpp.o"
+  "CMakeFiles/JSONCreator.dir/src/main.cpp.o.d"
+  "JSONCreator"
+  "JSONCreator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JSONCreator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
